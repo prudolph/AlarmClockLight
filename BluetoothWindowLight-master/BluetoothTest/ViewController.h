e9cba7881c5c20929b9231593a56cc41d7214bf9
prudolph <paul.e.rudolph@gmail.com>
2014-10-10 12:45:33 -0400

    worked on arduino code

---
+++ BluetoothWindowLight-master/BluetoothTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  BluetoothTest
+//
+//  Created by PRudolph on 10/7/14.
+//  Copyright (c) 2014 PRudolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "UARTPeripheral.h"
+@interface ViewController : UIViewController
+
+
+@end
+
