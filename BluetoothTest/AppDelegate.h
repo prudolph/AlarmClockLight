c98218eacc3abb2cae5bc056de689e9b16aec98b
Paul Rudolph <paul.e.rudolph@gmail.com>
2014-10-10 09:07:02 -0400

    added arduino

---
+++ BluetoothTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothTest
+//
+//  Created by PRudolph on 10/7/14.
+//  Copyright (c) 2014 PRudolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BluetoothTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BluetoothTest
+//
+//  Created by PRudolph on 10/7/14.
+//  Copyright (c) 2014 PRudolph. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
